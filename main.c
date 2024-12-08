e8d4f8baf2f90b8c715067425c301bda8c0815af
Lucas Lebugle <llebugle@fu-r2-p5.s19.be>
2024-12-08 16:08:31 +0100

    just tried some stuff with zed

--- main.c
+++ main.c
@@ -5,16 +5,23 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: llebugle <lucas.lebugle@student.s19.be>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/07 22:11:07 by llebugle          #+#    #+#             */
-/*   Updated: 2024/12/07 22:20:56 by llebugle         ###   ########.fr       */
+/*   Created: 2024/12/08 20:32:15 by llebugle          #+#    #+#             */
+/*   Updated: 2024/12/08 14:54:09 by llebugle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+#include <stdio.h>
 
 int	main(int ac, char **av)
 {
-	int i;
+	int	i;
 
+	i = 0;
+	i = 1;
+	i = 2;
+	i = 3;
+	printf("test");
+	printf("test");
 	return (0);
 }
--- pipex.h
+++ pipex.h
@@ -6,11 +6,18 @@
 /*   By: llebugle <lucas.lebugle@student.s19.be>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/07 22:12:11 by llebugle          #+#    #+#             */
-/*   Updated: 2024/12/07 22:13:07 by llebugle         ###   ########.fr       */
+/*   Updated: 2024/12/08 14:56:43 by llebugle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef pipex_h
-#define pipex_H
+#ifndef PIPEX_H
+# define PIPEX_H
+# include <stdio.h>
+
+typedef struct s_list
+{
+	int	n;
+}		t_list;
 
 #endif
+
